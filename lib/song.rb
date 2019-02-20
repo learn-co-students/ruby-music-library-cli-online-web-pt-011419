4e4f8caea56d842df20e3886b86276769a889435
blackwell <blackwellsmithiv@gmail.com>
2019-02-20 18:56:21 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,28 @@
+
+class Song 
+  attr_accessor :name, :artist
+  @@all = []
+  def initialize(name, artist = nil)
+    @name = name
+    @artist = artist
+  end 
+  
+  def self.all
+    @@all
+  end
+  
+  def self.destroy_all
+    @@all.clear
+  end
+   def save
+     @@all << self
+   end
+   def self.create(name)
+     new_song = Song.new(name)
+     new_song.save
+     new_song
+   end
+   def artist=(self)
+     artist.add_song(artist)
+   end
+end
