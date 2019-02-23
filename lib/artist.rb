69560e754d3e1410baeb2ce8e954810de3d301fc
A Glazer <avigayilglazer@gmail.com>
2019-02-23 18:48:42 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,10 +1,11 @@
 class Artist
-  attr_accessor :name
+  attr_accessor :name, :songs
   @@all = []
 
   def initialize(name)
     @name = name
     @@all << self
+    @songs = []
   end
 
   def self.all
