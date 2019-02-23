8128d867c542cdbd27a34fdd0fd1de4b795166a2
A Glazer <avigayilglazer@gmail.com>
2019-02-23 18:34:41 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,25 @@
+class Artist
+  attr_accessor :name
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.destroy_all
+    @@all.clear
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.create(artist)
+    Artist.new(artist)
+  end
+end
