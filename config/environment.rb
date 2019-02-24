aa7651324af5206aff838229f1d1f3d48b273c9e
A Glazer <avigayilglazer@gmail.com>
2019-02-24 11:21:34 +0000

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -4,10 +4,5 @@ Bundler.require
 module Concerns
 end
 
-require_all 'lib'
-# require_relative '../lib/concerns/findable'
-# require_relative '../lib/concerns/memorable'
-# require_relative '../lib/concerns/paramable'
-#
-# require_relative '../lib/artist.rb'
-# require_relative '../lib/song.rb'
+ require_all 'lib'
+ 
