af5fc93e3cb0d9ffadca70a410b35e2f7ceda324
A Glazer <avigayilglazer@gmail.com>
2019-02-24 19:24:00 +0000

    Done.

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -3,8 +3,40 @@ attr_accessor :path, :MusicImporter
 # @@MusicImporter = []
 
   def initialize(path = "./db/mp3s")
-    # @path = path
-    MusicImporter.new(path)
-    # @@MusicImporter << path
+    @path = MusicImporter.new(path).import
+  end
+
+  def call
+    puts "Welcome to your music library!"
+    puts "To list all of your songs, enter 'list songs'."
+    puts "To list all of the artists in your library, enter 'list artists'."
+    puts "To list all of the genres in your library, enter 'list genres'."
+    puts "To list all of the songs by a particular artist, enter 'list artist'."
+    puts "To list all of the songs of a particular genre, enter 'list genre'."
+    puts "To play a song, enter 'play song'."
+    puts "To quit, type 'exit'."
+    puts "What would you like to do?"
+    i = 0
+    loop do
+      input = gets.chomp
+      i += 1
+      if i == 4 || input == "exit"
+        break
+
+      end
+    end
+  end
+
+  def list_songs
+    lala = Song.all.uniq.sort_by! do |song|
+      song.name
+    end
+
+    lala.each.with_index(1) do |song, i|
+
+      puts "#{i}. #{song.artist.name} - #{song.name} - #{song.genre.name}"
+      # binding.pry
+    end
+
   end
 end
