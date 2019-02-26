0cd5917fad06ce8b2878727d6a30519634819915
Heather Kirk <hskirk@gmail.com>
2019-02-26 01:24:09 +0000

    Done.

--- lib/models/MusicLibraryController.rb
+++ lib/models/MusicLibraryController.rb
@@ -1,8 +1,6 @@
 class MusicLibraryController
-  
- # attr_accessor :path 
+  4
   def initialize (path = './db/mp3s')
-    #@path = path 
     MusicImporter.new(path).import 
   end 
   
@@ -17,21 +15,46 @@ class MusicLibraryController
     puts "To list all of the songs of a particular genre, enter 'list genre'."
     puts "To play a song, enter 'play song'."
     puts "To quit, type 'exit'."
-    puts "What would you like me to do?"
+    puts "What would you like to do?"
   input = gets.strip
   
-# # case input 
-#   # when "list songs"
-#   #  list_songs
-#   when "list artists"
-#     list_artists
-#   when "list genres"
-#         list_genres
-#         when "play song"
-#           play_song
-#           end 
+ case input 
+   when "list songs"
+    list_songs
+  when "list artists"
+    list_artists
+  when "list genres"
+    list_genres
+  when "play song"
+    play_song
+  when "list artist"
+    list_songs_by_artist
+  when "list genre"
+    list_songs_by_genre
+    end 
    end 
+  end 
+
+
+def list_songs 
+  Song.all.sort_by {|song| song.name}.each_with_index do |song, index|
+  puts "#{index +1 }. #{song.artist.name} - #{song.name} - #{song.genre.name}"
   end 
 end 
 
-
+def list_artists 
+  Artist.all.sort_by {|artist| artist.name}.each_with_index do |artist, index|
+    puts "#{index +1 }. #{artist.name}"
+  end
+ end 
+ 
+ def list_genres
+    Genre.all.sort_by {|genre| genre.name}.each_with_index do |genre, index|
+    puts "#{index +1 }. #{genre.name}"
+  end
+ end 
+ 
+ def list_songs_by_artist
+   puts "Please enter the name of an artist:"
+ end 
+end 
