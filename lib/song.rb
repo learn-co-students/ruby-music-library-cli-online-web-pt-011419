a56c63cf2dfe6767c63ae11ee1b09b41c0029620
Kelsey Low <kklowlow@gmail.com>
2019-02-27 21:20:05 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -23,6 +23,14 @@ class Song
     song
   end
   
+  def self.find_by_name(name)
+    self.all.find{|song| song.name == name}
+  end
+  
+  def self.find_or_create_by_name(name)
+    self.find_by_name(name) || self.create(name)
+  end
+  
   def save
     @@all << self
   end
