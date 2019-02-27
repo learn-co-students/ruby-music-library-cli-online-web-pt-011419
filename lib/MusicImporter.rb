e255f9b168914268160184d2fc206c191655f7a9
Kelsey Low <kklowlow@gmail.com>
2019-02-27 22:17:32 +0000

    Done.

--- lib/MusicImporter.rb
+++ lib/MusicImporter.rb
@@ -1,3 +1,19 @@
 class MusicImporter
+  attr_accessor :path, :song, :artist, :genre
   
+  def initialize(path)
+    @path = path
+  end
+  
+    # returns all imported files, deletes non-MP3s
+  def files 
+    Dir.entries(@path).select! {|entry| entry.end_with?(".mp3")}
+  end
+  
+    # imports files from the directory, instantiates song for each file
+  def import 
+    self.files.each do |filename|
+      Song.create_from_filename(filename)
+    end
+  end
 end
--- lib/song.rb
+++ lib/song.rb
@@ -27,8 +27,26 @@ class Song
     self.all.find{|song| song.name == name}
   end
   
-  def self.find_or_create_by_name(name)
+  def self.find_or_create_by_name(name) # runs #find_by_name or runs #create
     self.find_by_name(name) || self.create(name)
+  end
+  
+    # splits filenames into array, set variables to each index for each attribute
+    # sets artist and genre attributes to their objects using #find_or_create_by_name
+    # instantiates a new song based on the filename, complete with artist and genre
+  def self.new_from_filename(filename)
+    files = filename.split(" - ")
+    artist_name = files[0]
+    song_name = files[1]
+    genre_name = files[2].split(".mp3").join
+    artist = Artist.find_or_create_by_name(artist_name)
+    genre = Genre.find_or_create_by_name(genre_name)
+    self.new(song_name, artist, genre)
+  end
+  
+    # runs #new_from_filename and then saves the instance to @@all
+  def self.create_from_filename(filename)
+    self.new_from_filename(filename).save
   end
   
   def save
