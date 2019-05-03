59f4a569bd4de6c9b159add7b3baf933e9325f11
Dylan Michael Labat <dml252@live.com>
2019-05-03 18:26:54 +0000

    Automatically backed up by Learn

---
+++ lib/models/song.rb
@@ -0,0 +1,54 @@
+class Song
+  attr_accessor :name, :artist
+  @@all = []
+  
+  def initialize(name, artist = nil, genre = nil)
+    @name = name
+    self.artist = artist if artist != nil
+    self.genre = genre if genre !=nil
+  end
+  
+  def save
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def self.destroy_all
+    @@all.clear
+  end
+  
+  def self.create(name)
+    self.new(name).tap do |song|
+      song.save
+    end
+  end
+  
+  def artist=(artist)
+    @artist = artist
+    artist.add_song(self)
+  end
+  
+  def genre
+    @genre
+  end
+  
+  def genre=(genre)
+    @genre = genre
+    if !genre.songs.include?(self)
+      genre.songs << self
+    end
+  end
+  
+  def self.find_by_name(name)
+    self.all.find do |song|
+      song.name == name
+    end
+  end
+  
+  def self.find_or_create_by_name(name)
+    self.find_by_name(name) || self.create(name)
+  end
+end
